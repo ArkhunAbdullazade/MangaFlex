18d75c25f2e260cfd95d3277ca5729b1e4575dff
Arkhun Abdullazade <arkhun2008@gmail.com>
2024-03-14 16:26:22 +0400

    Create Project Base

---
+++ src/MangaFlex.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MangaFlex.Presentation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
